8942d087357e48c9a48496ba0fe95cbfdccb6818
Phan Thanh Bách <20521089@gm.uit.edu.vn>
2023-08-15 18:23:32 +0700

    Update App.js
    Add Splash.js, DangNhap.js, DangKy.js, QuenMatKhau.js to Stack.Navigator.
    Add login condition.

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
-export default function App() {
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+//import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { auth, setDoc, db, doc } from "./firebase";
+
+import Splash from "./src/screens/Splash";
+import BottomTab from "./src/components/BottomTab";
+import DangNhap from "./src/screens/DangNhap";
+import DangKy from "./src/screens/DangKy";
+import QuenMatKhau from "./src/screens/QuenMatKhau";
+// import Menu from './src/screen/Menu';
+// import OrderConfirm from './src/screen/Checkout/OrderConfirm';
+// import OrderHistory from './src/screen/OrderHistory';
+// import OrderTracking from './src/screen/OrderTracking';
+// import OrderManagement from './src/screen/OrderManagement';
+// import Home from './src/screen/Home';
+// import FoodDetail from './src/screen/FoodDetail';
+// import ReviewDescription from './src/screen/Rating/ReviewDescription';
+// import Account from './src/screen/Account';
+
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const unsubscribe = auth.onAuthStateChanged((user) => {
+    if (user) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  });
+
+  const Stack = createStackNavigator();
+  console.log(Stack);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer independent={true}>
+        <SafeAreaView style={{ flex: 1 }}
+        >
+          {/* <StatusBar style='light' /> */}
+          <Stack.Navigator
+            initialRouteName="Splash"
+            screenOptions={{ headerShown: false }}
+          >
+            {loggedIn ? (
+              <Stack.Screen name="BottomTab" component={BottomTab} />
+            ) : (
+              <Stack.Group>
+                <Stack.Screen name="Splash" component={Splash} />
+                <Stack.Screen name="DangNhap" component={DangNhap} />
+                <Stack.Screen name="DangKy" component={DangKy} />
+                <Stack.Screen name="QuenMatKhau" component={QuenMatKhau} />
+              </Stack.Group>
+            )}
+          </Stack.Navigator>
+        </SafeAreaView>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
