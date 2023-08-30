7c7b7cd021b3f7a07c9c4aa78bc9eb578dbbbf7b
Phan Thanh Bách <20521089@gm.uit.edu.vn>
2023-08-30 17:31:28 +0700

    Create Founder.js

---
+++ src/components/HomeScreen/Founder.js
@@ -0,0 +1,8 @@
+const founders = [
+    {id: '1', name: 'Hồng Minh', image: require('../../assets/icons/HomeIcons/minh.png') },
+    {id: '2', name: 'Thanh Bách', image: require('../../assets/icons/HomeIcons/bach.png') },
+    {id: '3', name: 'Hoài Nam', image: require('../../assets/icons/HomeIcons/hoainam.png') },
+    {id: '4', name: 'Vân Anh', image: require('../../assets/icons/HomeIcons/vananh.png') }, 
+];
+
+export default founders;
