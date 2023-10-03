118805b85787e9c7d8fe8ae049593dd8f04b3be3
Phan Thanh Bách <20521089@gm.uit.edu.vn>
2023-10-03 16:31:42 +0700

    Create FoodDetail.js

---
+++ src/screens/FoodDetail.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FoodDetail = () => {
+  return (
+    <View>
+      <Text>FoodDetail</Text>
+    </View>
+  )
+}
+
+export default FoodDetail
