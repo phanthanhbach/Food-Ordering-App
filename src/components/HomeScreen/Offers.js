e884625943780f79c17787982751e945ada4f0d1
Phan Thanh Bách <20521089@gm.uit.edu.vn>
2023-10-03 15:08:45 +0700

    Create Offers.js
    Add static data

---
+++ src/components/HomeScreen/Offers.js
@@ -0,0 +1,7 @@
+const offers = [
+    {id: '1', name: 'hi',  image: require('../../assets/voucher1.png') },
+    {id: '2',  name: 'ha', image: require('../../assets/voucher2.png') },
+    {id: '3', name: 'ho',  image: require('../../assets/voucher1.png') }, 
+];
+
+export default offers;
